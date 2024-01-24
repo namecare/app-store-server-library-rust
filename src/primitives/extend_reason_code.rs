96cb7245a6a5ab847b1fb0d41bc38c7fe350aa5c
tikhop <tikhop@gmail.com>
2024-01-24 02:16:16 +0100

    fix: Properly deserialize int to enum

--- src/primitives/extend_reason_code.rs
+++ src/primitives/extend_reason_code.rs
@@ -1,9 +1,10 @@
-use serde::{Deserialize, Serialize};
+use serde_repr::{Serialize_repr, Deserialize_repr};
 
 /// The code that represents the reason for the subscription-renewal-date extension.
 ///
 /// [extendReasonCode](https://developer.apple.com/documentation/appstoreserverapi/extendreasoncode)
-#[derive(Debug, Clone, Deserialize, Serialize, Hash, PartialEq, Eq)]
+#[derive(Debug, Clone, Deserialize_repr, Serialize_repr, Hash, PartialEq, Eq)]
+#[repr(u8)]
 pub enum ExtendReasonCode {
     Undeclared = 0,
     CustomerSatisfaction = 1,
--- src/primitives/lifetime_dollars_refunded.rs
+++ src/primitives/lifetime_dollars_refunded.rs
@@ -1,9 +1,10 @@
-use serde::{Deserialize, Serialize};
+use serde_repr::{Serialize_repr, Deserialize_repr};
 
 /// A value that indicates the dollar amount of refunds the customer has received in your app, since purchasing the app, across all platforms.
 ///
 /// [lifetimeDollarsRefunded](https://developer.apple.com/documentation/appstoreserverapi/lifetimedollarsrefunded)
-#[derive(Debug, Clone, Deserialize, Serialize, Hash, PartialEq, Eq)]
+#[derive(Debug, Clone, Deserialize_repr, Serialize_repr, Hash, PartialEq, Eq)]
+#[repr(u8)]
 pub enum LifetimeDollarsRefunded {
     Undeclared = 0,
     ZeroDollars = 1,
